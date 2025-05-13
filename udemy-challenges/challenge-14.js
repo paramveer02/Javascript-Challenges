98052fa0cfe871f9bd285678c609222fa66571f1
Paramveer <paramveermarwahafc@gmail.com>
2025-05-13 15:41:56 +0200

    added challenge 14

---
+++ udemy-challenges/challenge-14.js
@@ -0,0 +1,23 @@
+/*
+Coding Challenge #14
+
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+ calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+*/
